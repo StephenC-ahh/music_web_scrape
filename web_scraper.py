57b5b9f60bf10fb27a458ea56c10593904564f7c
Stephen Coatsworth <59412372+StephenC-ahh@users.noreply.github.com>
2022-01-27 15:38:03 +0000

    email settings added

--- web_scraper.py
+++ web_scraper.py
@@ -1,4 +1,6 @@
+from xmlrpc.client import SERVER_ERROR
 import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 from urllib import response
 from urllib3 import PoolManager
 import requests #http requests
@@ -18,7 +20,7 @@ now = datetime.datetime.now()
 content = ''
 
 def extract_news(url):
-    print('Extrating Udiscover Music ReDiscovered Albums...')
+    print('Extracting Udiscover Music ReDiscovered Albums...')
     cnt = ''
     cnt +=('<b>Udiscover Rediscovered Albums:<b>\n'+'<br>'*50+'<br>')
     response = requests.get(url, verify=False)
@@ -34,10 +36,31 @@ content += cnt
 content += ('<br>------<br>')
 content += ('<br><br>End of Message')
 
-import sys
-print(content) 
-original_stdout = sys.stdout
-with open('filename.txt', 'w') as f:
-    sys.stdout = f
-    print(content)
-    sys.stdout = original_stdout
+print('Composing Email...')
+
+SERVER = 'smtp.gmail.com' # your smtp server
+PORT = 587 #your port number
+FROM = '' #your email id
+TO = '' # recipient email, can be a list
+PASS = input('Enter your Email password') #your email id password'
+
+msg = MIMEMultipart()
+
+msg['Subject'] = 'UDiscover Music Rediscovered Albums' + ' ' + ' ' + str(now.day) + '_' + str(now.month) + '_' + str(now.year)
+
+msg['From'] = FROM
+
+msg['To'] = TO
+
+msg.attach(MIMEText(content, 'html'))
+
+print('Intiating Server...')
+
+server = smtplib.SMTP(SERVER, PORT)
+server.set_debuglevel(1)
+server.ehlo()
+server.starttls()
+server.login(FROM)
+server.send_message(TO)
+
+print('Email Sent')
